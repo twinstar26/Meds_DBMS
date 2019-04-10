cc213fa5df78f004598c60720bcbe0c94c1a5db4
DivyPatel9881 <divy9881@gmail.com>
2019-04-10 19:07:17 +0530

    More fields to patient data added

--- Populating_Code/patient_data.py
+++ Populating_Code/patient_data.py
@@ -1,5 +1,6 @@
 import mysql.connector as mysql
 import pandas as pd
+import random as rd
 
 my_db = mysql.connect(host="localhost",user="Divy",passwd="pwd")
 
@@ -7,17 +8,24 @@ my_cur = my_db.cursor()
 
 my_cur.execute("use Hospital_Database")
 my_cur.execute("drop table patients")
-my_cur.execute("create table Patients(Id int (10) primary key,Name varchar(50),Age int(10),Height int(10),Weight int(10),Gender varchar(2),Doctor_Id varchar(256));")
+my_cur.execute("create table Patients(Id int (10) primary key,Name varchar(50),Age int(10),Height int(10),Weight int(10),Gender varchar(2),Blood_Group varchar(3),Illness varchar(50),Medication_Drug varchar(100),Doctor_Id varchar(256));")
 
 df_pat = pd.read_csv("../Dataset/patients.csv")
 df_doc = pd.read_csv("../Dataset/doctors.csv")
+df_phar = pd.read_csv("../Dataset/pharmacy.csv")
 
 keys = []
 for k in df_pat:
 	keys.append(k)
 
+blood_grp = ["AB","A","B","O"]
+illness = ["Ischaemic heart disease","Cancer","Diabetes","AIDS","Parkinson disease","Chronic lung","Alzheimer’s disease","Paralysis"]
+
 keys.append("NAME")
 keys.append("UNIQ_ID")
+keys.append("Blood_grp")
+keys.append("Illness")
+keys.append("Drug")
 
 for i in range(1000):
 	Tuple = [] 
@@ -33,9 +41,16 @@ for i in range(1000):
 			Tuple.append(str(df_doc[key][i]))
 		elif key=="UNIQ_ID":
 			Tuple.append(str(df_doc[key][i+3000]))
+		elif key=="Blood_grp":
+			Tuple.append(blood_grp[rd.randrange(1,len(blood_grp)+1)-1])
+		elif key=="Illness":
+			Tuple.append(illness[rd.randrange(1,len(illness)+1)-1])
+		elif key=="Drug":
+			Tuple.append(str(df_phar[key][rd.randrange(1,len(df_phar)+1)-1]))
 		else:
 			Tuple.append(str(df_pat[key][i]))
 	Tuple = tuple(Tuple)
-	my_cur.execute("insert into Patients (Id, Age,Gender,Weight,Height,Name,Doctor_Id) Values(%s,%s,%s,%s,%s,%s,%s);",Tuple)
+	print("hello")
+	my_cur.execute("insert into Patients (Id, Age,Gender,Weight,Height,Name,Doctor_Id,Blood_Group,Illness,Medication_Drug) Values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",Tuple)
 
 my_db.commit()
