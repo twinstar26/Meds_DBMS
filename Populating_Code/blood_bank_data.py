4599dfe5559dc32dcafcb631efe64a75a394aea9
DivyPatel9881 <divy9881@gmail.com>
2019-04-11 17:30:51 +0530

    Blood Bank populating code added

--- Populating_Code/blood_bank_data.py
+++ Populating_Code/blood_bank_data.py
@@ -2,55 +2,25 @@ import mysql.connector as mysql
 import pandas as pd
 import random as rd
 
-my_db = mysql.connect(host="localhost",user="Divy",passwd="pwd")
+my_db = mysql.connect(host="localhost",user="Divy",passwd="dhruti")
 
 my_cur = my_db.cursor()
 
 my_cur.execute("use Hospital_Database")
-my_cur.execute("drop table patients")	
-my_cur.execute("create table Patients(Id int (10) primary key,Name varchar(50),Age int(10),Height int(10),Weight int(10),Gender varchar(2),Blood_Group varchar(3),Illness varchar(50),Medication_Drug varchar(100),Doctor_Id varchar(256));")
+my_cur.execute("drop table blood_bank")	
+my_cur.execute("create table Blood_Bank(Id int (10) primary key,Recipents varchar(50),Donors varchar(50),Blood_Group varchar(50),Quantity varchar(50));")
 
-df_pat = pd.read_csv("../Dataset/patients.csv")
-df_doc = pd.read_csv("../Dataset/doctors.csv")
-df_phar = pd.read_csv("../Dataset/pharmacy.csv")
+df_blood_bank = pd.read_csv("../Dataset/blood_bank.csv")
 
 keys = []
-for k in df_pat:
+for k in df_blood_bank:
 	keys.append(k)
-
-blood_grp = ["AB","A","B","O"]
-illness = ["Ischaemic heart disease","Cancer","Diabetes","AIDS","Parkinson disease","Chronic lung","Alzheimer’s disease","Paralysis"]
-rhesus = ["+","-"]
-
-keys.append("NAME")
-keys.append("UNIQ_ID")
-keys.append("Blood_grp")
-keys.append("Illness")
-keys.append("Drug")
 
 for i in range(1000):
 	Tuple = [] 
 	for key in keys:
-		if key=="age":
-			Tuple.append(str(int(df_pat[key][i]) // 366))
-		elif key=="gender":
-			if str(df_pat[key][i])=="1":
-				Tuple.append("M")
-			else:
-				Tuple.append("F")
-		elif key=="NAME":
-			Tuple.append(str(df_doc[key][i]))
-		elif key=="UNIQ_ID":
-			Tuple.append(str(df_doc[key][i+3000]))
-		elif key=="Blood_grp":
-			Tuple.append(blood_grp[rd.randrange(1,len(blood_grp)+1)-1]+rhesus[rd.randrange(1,len(rhesus)+1)-1])
-		elif key=="Illness":
-			Tuple.append(illness[rd.randrange(1,len(illness)+1)-1])
-		elif key=="Drug":
-			Tuple.append(str(df_phar[key][rd.randrange(1,len(df_phar)+1)-1]))
-		else:
-			Tuple.append(str(df_pat[key][i]))
+		Tuple.append(str(df_blood_bank[key][i]))
 	Tuple = tuple(Tuple)
-	my_cur.execute("insert into Patients (Id, Age,Gender,Height,Weight,Name,Doctor_Id,Blood_Group,Illness,Medication_Drug) Values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",Tuple)
+	my_cur.execute("insert into Blood_Bank (Id, Recipents,Donors,Blood_Group,Quantity) Values(%s,%s,%s,%s,%s);",Tuple)
 
 my_db.commit()
