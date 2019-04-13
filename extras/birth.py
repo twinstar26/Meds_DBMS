fa1d04571d0a9669962d519f23d72d60bb850c31
twinstar26 <adveer_b17@it.vjti.ac.in>
2019-04-13 19:04:38 +0530

    added birth dataset

--- extras/birth.py
+++ extras/birth.py
@@ -2,8 +2,8 @@ import pandas
 import numpy
 import random
 
-
-csv = pandas.read_csv(filepath_or_buffer='~/projects/Meds_DBMS/Dataset/hospitals.csv', encoding='utf-8')
+#csv = pandas.read_csv(filepath_or_buffer='~/projects/Meds_DBMS/Dataset/birth.csv', encoding='utf-8')
+#csv = pandas.DataFrame
 births = []
 for o in range(6485):
     births.append(random.randint(0,280))
@@ -13,5 +13,9 @@ for o in range(6485):
 defective = []
 for o in range(6485):
     defective.append(random.randint(0,28))
-csv.insert(value=births, loc=8, column='DISEASE_OUTBREAK',allow_duplicates=True)
+csv = pandas.DataFrame(births)
+#csv.insert(value=births, loc=0, column='FOETUS_BORN',allow_duplicates=True)
+csv.insert(value=gender, loc=1, column='GENDER',allow_duplicates=True)
+csv.insert(value=defective, loc=2, column='UNHEALTHY_FOETUS',allow_duplicates=True)
 
+csv.to_csv(path_or_buf='~/projects/Meds_DBMS/Dataset/birth.csv', encoding='utf-8',sep=',', header=True, )
